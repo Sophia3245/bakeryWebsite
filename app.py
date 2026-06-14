ec8e4b534edf179e2616f3734c28d9a1ce08cc46
Sophia3245 <sophiakidney@gmail.com>
2026-06-14 12:05:21 +0100

    push5

--- app.py
+++ app.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/aboutUs')
+def aboutUs():
+    return render_template('aboutUs.html')
+
+@app.route('/conUs')
+def conUs():
+    return render_template('contactUs.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('FAQ.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/termsAndCon')
+def TaC():
+    return render_template('termsAndCon.html')
+
 if __name__ == '__main__':
     app.run()
